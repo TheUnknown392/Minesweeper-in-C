f108d62e45409805df24f008265a4c3cd9569b26
TheUnknown392 <rabinamahrjan392@gmail.com>
2024-10-03 21:29:09 +0545

    unique files
    start attempt to redability and shorten code, corrected RxC

--- Minesweeper.c
+++ Minesweeper.c
@@ -1,15 +1,18 @@
-//\nType [s <difficulty>] to start a game. [s] for highest score in each game\n[q q] to quit anywhere in during game\n[d <row>,<colunm> to dig]\n[f <row>,<colunm> to flag]\n[r <row>,<colunm> to remove from number]
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-#define C 16
+// sets row and colunm of matrix
 #define R 16
+#define C 16
+// three levels of dfficulty
 #define EASY 15
-#define MEDIUM 20
-#define HARD 25
+#define MEDIUM 22
+#define HARD 30
+// the extention of file
+#define fileExten "MINESWEEPERPLAYER.txt"
 
 typedef struct input{
     char f;
@@ -28,14 +31,18 @@ typedef struct score{
     int hTime;
 }score;
 
+typedef struct playCount{
+    int totalPlayed,totalwon,lostOrAbandoned;
+}playCount;
+
 typedef struct playerInfo{
     char name[50],password[50];
-    int id;
     score pScore;
+    playCount pCount;
 }playerInfo;
 
 time_t start,end;
-char game[C][R];
+char game[R][C];
 char ch[3][3];
 
 void rst();
@@ -50,12 +57,10 @@ void redBold();
 void cyan();
 
 void seeTime(t *a,int info);
-int minesweeper(char c);
+int minesweeper(char c,playerInfo *active);
 playerInfo mainMenu();
-int newID();
-int signin();
+int signup();
 int newUser(playerInfo *data);
-playerInfo searchID(int id , char password[]);
 playerInfo login();
 int innit();
 
@@ -70,22 +75,20 @@ void dig(int a,int b);
 void flag(input *in);
 void rFlag(input *in);
 void quit(input *in);
-void rNum(int a, int b);
+int rNum(int a, int b);
 void spreadOut(int x, int y);
-int gameover(int count);
-int countBomb();
+int gameover(playerInfo *active);
 
 int main(){
     playerInfo active = mainMenu();
     printf("\nlogged in with\nUser:%s\n\n",active.name);
     char g='a';
-
+    int Time;
     do{
         do{
             printf("Difficulty: e=easy m=medium h=hard\nType [<difficulty>] to start a game.\n[s] for highest score in each game\n");
             scanf(" %c",&g);
         }while( (g!='e')&&(g!='m')&&(g!='h')&&(g!='s'));
-
         if(g=='s'){
             t scoreTime;
             seeTime(&scoreTime, active.pScore.eTime);
@@ -96,11 +99,49 @@ int main(){
             printf("h: %d:%d:%d\n",scoreTime.hh,scoreTime.mm,scoreTime.ss);
         }
     }while(g=='s');
+    minesweeper(g,&active);
+    Time=difftime(end,start);
+    switch(g){
+        case 'e':
+            if(Time<active.pScore.eTime){
+                active.pScore.eTime=Time;
+            }
+            break;
+        case 'm':
+            if(Time<active.pScore.mTime){
+                active.pScore.mTime=Time;
+            }
+            break;
+        case 'h':
+            if(Time<active.pScore.hTime){
+                active.pScore.hTime=Time;
+            }
+            break;
+        default:
+            printf("Error in moddifying high score");
+        break;
+    }
 
-    minesweeper(g);
-
+    char temp[100];
+    strcpy(temp,active.name);
+    strcat(temp,fileExten);
+    FILE*fp=fopen(temp,"w");
+    fprintf(
+        fp, 
+        "%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        active.name,
+        active.password,
+        active.pScore.eTime,
+        active.pScore.mTime,
+        active.pScore.hTime,
+        active.pCount.totalPlayed,
+        active.pCount.totalwon,
+        active.pCount.totalPlayed-active.pCount.totalwon
+    );
+    fclose(fp);
     return 0;
 }
+
 
 
 
@@ -117,7 +158,7 @@ playerInfo mainMenu(){
             return login();
             break;
         case 2:
-            signin();
+            signup();
             goto up;
             break;
         case 3:
@@ -137,119 +178,115 @@ playerInfo mainMenu(){
     }
 }
 
-int newID(){
-    int i,temp=0;
-    playerInfo search;
-    FILE *fp;
-    fp = fopen("userdata.dat","r");
-    if (fp == NULL){
-        printf("error in opening userdata.dat file -> signin()\nor userdata.dat file missing.");
-        exit(1);
-    }
 
-    while(fscanf(fp,"%d\t%s\t%s\t%d\t%d\t%d\n",&search.id,&search.name,&search.password,&search.pScore.eTime,&search.pScore.mTime,&search.pScore.hTime)!=EOF){ // inputs formatted value to struct
-        if(search.id>temp){
-            temp=search.id; // cheaks for the highest ID
-        }
-    }
-
-    fclose(fp);
-    return temp+1; // returns new id
-
-}
-
-int signin(){
+int signup(){
     playerInfo newSignin;    
 
-    printf("Input your name(no numbers): ");
-    scanf("%s",&newSignin.name);
-    printf("Input your New password(no numbers): ");
-    scanf("%s",&newSignin.password);
-    newSignin.id=newID(); //gets unique id
+    printf("Input your name (no numbers): ");
+    scanf("%s", newSignin.name);  
 
+    printf("Input your New password (no numbers): ");
+    scanf("%s", newSignin.password);  
 
-    newUser(&newSignin); //inputs newUser into file
+    char temp[100];  
+    strcpy(temp, newSignin.name);
+    strcat(temp, fileExten);
 
-    printf("\nYou have made your acc.\nPlease remember your ID: %d\n", newSignin.id);
-    return 1;
-}
-
-int newUser(playerInfo *data){
     FILE *fp;
-    fp = fopen("userdata.dat","a+");
-    if(fp==NULL){
-        printf("Error opening userdata.dat file -> newUser()\nor userdata.dat file missing.\n");
+    fp = fopen(temp, "w");
+    if(fp == NULL){
+        printf("Error opening file for user -> signup()\n");
         return 0;
     }
-    fprintf(fp,"%d\t%s\t%s\t%d\t%d\t%d\n",data->id,data->name,data->password,0,0,0);
+
+    fprintf(
+        fp, 
+        "%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        newSignin.name,
+        newSignin.password,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+    );
+
     fclose(fp);
+
+    printf("\nYou have created your account.\nPlease remember your Username.\n");
     return 1;
 }
 
 
-playerInfo searchID(int id , char password[]){
-    playerInfo search;
-    FILE * fp;
-    fp=fopen("userdata.dat","r");
-    if(fp==NULL){
-        printf("Error opening userdata.dat file -> searchID()\nor userdata.dat file missing.\n");
-    }
-    while(fscanf(fp,"%d\t%s\t%s\t%d\t%d\t%d\n",&search.id,&search.name,&search.password,&search.pScore.eTime,&search.pScore.mTime,&search.pScore.hTime)!=EOF){
-        if((id==search.id)&&(strcmp(password,search.password)==0)){ // if given id and password are same returns the unique data of file 
-            return search;
-        }
-    }
-
-    printf("\nError in searchID()\nno similar combination of user log in and password");
-    exit(1);
-}
 
 playerInfo login(){
-    int id;
-    char password[50];
-    printf("Input your ID: ");
-    scanf("%d",&id);
+    char password[50],userName[50];
+    printf("Input your Username: ");
+    scanf("%s",&userName);
     printf("Input your password: ");
     scanf("%s",&password);
-    return searchID(id,password);
+        playerInfo search;
+    char temp[100];
+    strcpy(temp, userName);
+    strcat(temp, fileExten);
+
+    FILE *fp = fopen(temp, "r");
+    if(fp == NULL){
+        printf("Error opening file for user -> searchUser()\n");
+        exit(0);
+    }
+
+    fscanf(
+        fp, 
+        "%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        search.name,
+        search.password,
+        &search.pScore.eTime,
+        &search.pScore.mTime,
+        &search.pScore.hTime,
+        &search.pCount.totalPlayed,
+        &search.pCount.totalwon,
+        &search.pCount.lostOrAbandoned
+    );
+    fclose(fp);
+
+    if(strcmp(password, search.password) == 0){
+        return search;
+    } else {
+        printf("\nIncorrect password or username -> searchUser()\n");
+        exit(0);
+    }
 }
 
 int innit(){
     FILE *fp;
-    fp=fopen("userdata.dat","w");
+    char temp[50];
+    strcpy(temp,"TheUnknown");
+    strcat(temp,fileExten);
+    fp=fopen(temp,"w");
     if(fp==NULL){
-        printf("Some error innitializing -> innit()");
         return 0;
     }
-    fprintf(fp,"%d\t%s\t%s\t%d\t%d\t%d\n",100,"TheUnknown","TheUnknown",0,0,0);
+    fprintf(fp,"%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\n","TheUnknown","TheUnknown",0,0,0,0,0,0);
     fclose(fp);
     return 1;
 }
 
-void seeTime(t *a,int info){
-    a->ss=info;
-    while(a->hh>=24||a->mm>=60||a->ss>=60){
-        if(a->ss>=60){
-            a->ss=(a->ss-60);
-            a->mm=(a->mm+1);
-        }else if(a->mm>=60){
-            a->mm=(a->mm-60);
-            a->hh=(a->hh+1);
-        }
-        else if(a->hh>=24){
-            a->hh=00;
-        }
-        else{
-            printf("Error in seeTime()");
-        }
-    }
+void seeTime(t *a, int info) {
+    a->hh = info / 3600;         
+    info = info % 3600;         
+    a->mm = info / 60;  
+    a->ss = info % 60;          
 }
 
 
 
-int minesweeper(char c){
+
+int minesweeper(char c,playerInfo *active){
     int bombAmt;
     input in;
+    int gameStatus;
     rstMatrix();
     display();
     red();
@@ -260,53 +297,43 @@ int minesweeper(char c){
     }while(in.f!='D');
     srand(time(NULL));
     bomb(&in,c);
-    bombAmt=countBomb();
     insertNumber();
     spreadOut(in.x,in.y);
+    active->pCount.totalPlayed+=1;
+    time(&start);
     do{
         display();
         userInput(&in);
         cheakInput(&in);
-    }while(gameover(bombAmt));
+    }while(gameover(active));
     display();
     printf("\n\nCongratulations! you have won the game!\n\n\n");
-    
-}
-int countBomb(){
-    int count=0;
-    for(int i=0;i<R;i++){
-        for(int j=0;j<C;j++){
-            if(game[i][j]=='B'){
-                count+=1;
-            }
-        }
-    }
-    return count;
 }
 void rstMatrix(){
 
-    for(int i=0;i<R;i++){
-        for(int j=0;j<C;j++){
+    for(int i=0;i<C;i++){
+        for(int j=0;j<R;j++){
             game[i][j]='W';
         }
     }
 
-    for(int i=1;i<R;i++){
-        for(int j=0;j<=C-1;j++){
+    for(int i=1;i<C;i++){
+        for(int j=0;j<=R-1;j++){
                 game[i][j]='G';           
         }
     }
 }
 
 void userInput(input *in){
-    do{
-        printf("\n [Q 0,0] to quit anywhere in during game\n[D <row>,<colunm> to dig]\n[F <row>,<colunm> to flag]\n[R <row>,<colunm> to remove from number]\n[r <row>,<colunm] to remove flag\ne.g. D 1,0 to dig the top left thing\n\n");
-        scanf(" %c %d,%d",&in->f,&in->x,&in->y);
-    }while(in->f!='Q'&&in->f!='D'&&in->f!='F'&&in->f!='R'&&in->f!='Q'&&in->f!='r'&&in->x>=0&&in->x<=10&&in->y>=0&&in->y<=9);
-    if(in->f=='Q'&&in->x==0&&in->y==0){
-        printf("You have exited the game");
-        exit(0);
-    }
+        do{
+            printf("\n [Q 0,0] to quit anywhere in during game\n[D <row>,<colunm> to dig]\n[F <row>,<colunm> to flag]\n[C <row>,<colunm>] to remove from number\n[r <row>,<colunm] to remove flag\ne.g. D 1,0 to dig the top left thing\n\n");
+            scanf(" %c %d,%d",&in->f,&in->x,&in->y);
+        }while(in->f!='Q'&&in->f!='D'&&in->f!='F'&&in->f!='C'&&in->f!='Q'&&in->f!='r'&&in->x>=0&&in->x<=10&&in->y>=0&&in->y<=9);
+        
+        if(in->f=='Q'&&in->x==0&&in->y==0){
+            printf("You have exited the game");
+            exit(0);
+        }
 }
 
 void surrounding(int c, int r){
@@ -324,7 +351,7 @@ void surrounding(int c, int r){
     }
 
     // Check bottom boundary
-    if(pc >= C){
+    if(pc >= R){
         ch[2][0] = 'Z';
         ch[2][1] = 'Z';
         ch[2][2] = 'Z';
@@ -338,7 +365,7 @@ void surrounding(int c, int r){
     }
 
     // Check right boundary
-    if(pr >= R-1){
+    if(pr >= C-1){
         ch[0][2] = 'Z';
         ch[1][2] = 'Z';
         ch[2][2] = 'Z';
@@ -375,8 +402,8 @@ void surrounding(int c, int r){
 }
 void insertNumber(){
     int i,j,k,l,count=0;
-    for(i=0;i<C;i++){ // cycles through all matrix except there is a bomb
-        for(j=0;j<R;j++){
+    for(i=0;i<R;i++){ // cycles through all matrix except there is a bomb
+        for(j=0;j<C;j++){
             if(game[i][j]!='B'){
                 surrounding(i,j); // takes surrounding info
                 for(k=0;k<3;k++){
@@ -396,25 +423,25 @@ void insertNumber(){
     
 }
 void cheakInput(input *in){
-    switch(in->f){
-        case 'D':
-        dig(in->x,in->y);
-        break;
-        case 'F':
-        flag(in);
-        break;
-        case 'R':
-        rNum(in->x,in->y);
-        break;
-        case 'r':
-        rFlag(in);
-        break;
-        case 'Q':
-        quit(in);
-        break;
-        default:
-        break;
-    }
+        switch(in->f){
+            case 'D':
+            dig(in->x,in->y);
+            break;
+            case 'F':
+            flag(in);
+            break;
+            case 'C':
+            rNum(in->x,in->y);
+            break;
+            case 'r':
+            rFlag(in);
+            break;
+            case 'Q':
+            quit(in);
+            break;
+            default:
+            break;
+        }
 }
 void dig(int a, int b){
     if(game[a][b]=='B'){ // digs a bomb
@@ -471,20 +498,41 @@ void rFlag(input *in){
         exit(0);
     }
 }
-void rNum(int a, int b){
+
+int rNum(int a, int b){
     if(game[a][b]>='1'&&game[a][b]<='9'){
-        dig(a-1, b-1);  // Top-left
-        dig(a-1, b);    // Top
-        dig(a-1, b+1);  // Top-right
-        dig(a, b-1);    // Left
-        dig(a, b+1);    // Right
-        dig(a+1, b-1);  // Bottom-left
-        dig(a+1, b);    // Bottom
-        dig(a+1, b+1);  // Bottom-right
-    }else{
-        printf("\nYou can only remove from numbers\n");
+        int countFlag=0;
+        surrounding(a,b);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(ch[i][j]!='Z'){
+                    if((ch[i][j]>='n'&&ch[i][j]<='v')||ch[i][j]=='b'||ch[i][j]=='g'){
+                        countFlag+=1;// counts flags in surrounding.
+                    }
+                }
+            }
+        }
+
+        if(countFlag>=/*atoi(&game[a][b])*/game[a][b]-'0'){ // turns character number into an integer
+            dig(a-1, b-1);  // Top-left
+            dig(a-1, b);    // Top
+            dig(a-1, b+1);  // Top-right
+            dig(a, b-1);    // Left
+            dig(a, b+1);    // Right
+            dig(a+1, b-1);  // Bottom-left
+            dig(a+1, b);    // Bottom
+            dig(a+1, b+1);  // Bottom-right
+	        return 1;
+        }else{
+            printf("\nYou can only remove from numbers if you have proper flag\nor error in ->rNum(countFlag: %d, game[a][b] num=%d)\n",countFlag,game[a][b]-'0');
+            return 0;
+        }
+	}else{
+    	printf("\nYou can only remove from numbers\nor error in ->rNum()\n");
+	    return 2;
     }
 }
+
 int lcharAscii(char c){
     if(c>='N'&&c<='V'){// changes lower n to ascii numbers
         return c-29; 
@@ -494,7 +542,7 @@ int lcharAscii(char c){
 
 void spreadOut(int x, int y) {
     // Base condition: check if out of bounds
-    if (x < 1 || x >= C || y < 0 || y >= R-1) {
+    if (x < 1 || x >= R || y < 0 || y >= C-1) {
         return;
     }
 
@@ -531,26 +579,20 @@ void spreadOut(int x, int y) {
     // spreadOut(x+1, y+1);  // Bottom-right
 }
 
-int gameover(int count){
-    int i,j,bFlagNum=0,numGF=0,numNum=0;
-    for(i=1;i<C-1;i++){
-        for(j=0;j<=R-2;j++){
-            if(game[i][j]=='b'){
-                bFlagNum+=1;
-            }
-            if(game[i][j]=='G'||game[i][j]=='g'||(game[i][j]=='N'&&game[i][j]=='V')){
+int gameover(playerInfo *active){
+    int i,j,numGF=0;
+    for(i=1;i<R;i++){
+        for(j=0;j<=C-2;j++){
+            if(game[i][j]=='G'||game[i][j]=='g'||(game[i][j]>='N'&&game[i][j]<='V')){
                 numGF+=1;
-            }
-            if((game[i][j]=='n'&&game[i][j]=='v')){
-                numNum+=1;
             }
         }
     }
-    if(bFlagNum==count&&numGF==0&&numNum==0){
-        //printf("\n\n\nFlagged bomb: %d number of G/gf: %d Number of flagged number: %d\n\n\n",bFlagNum,numGF,numNum); to test
+    if(numGF==0){// if everything that was diggable is digged
+        active->pCount.totalwon+=1;
+        time(&end);
         return 0;
     }else{
-        //printf("\n\n\nFlagged bomb: %d number of G/gf: %d Number of flagged number: %d\n\n\n",bFlagNum,numGF,numNum);
         return 1;
     }
 
@@ -559,13 +601,13 @@ int gameover(int count){
 
 void display(){//displayes the current matrix
     int i,j;
-    for(i=0;i<C;i++){
+    for(i=0;i<R;i++){
         white();
         printf("%d",i);
-        for(j=0;j<R-1;j++){
+        for(j=0;j<C-1;j++){
             if(i==0){
                 printf("\t%d",j);
-                if(j==R-2){
+                if(j==C-2){
                     printf("\n");
                 }
             }
@@ -633,16 +675,37 @@ void bomb(input *in, char level){
         exit(0);
     }
     do{
-        tempR=rand()%(R-1);
-        tempC=rand()%(C-2) + 1;
-        if((in->x!=tempC&&in->y!=tempR)){ // not bomb in first press
-            if(game[tempC][tempR]!='B'&&game[tempC][tempR]!='W'){ // if the value is already B there
-                game[tempC][tempR]='B';
+        tempC=rand()%(C-1);
+        tempR=rand()%(R-1) + 1;
+        if((in->x!=tempR&&in->y!=tempC)){ // not bomb in first press
+            if(game[tempR][tempC]!='B'&&game[tempR][tempC]!='W'){ // if the value is already B there
+                game[tempR][tempC]='B';
                 --nBombs;
             }
         }
     }while(nBombs>0);
 }
+
+// void inputFile(playerInfo active,int winLoss){
+//     char temp[100];
+//     strcpy(temp,active.name);
+//     strcat(temp,fileExten);
+//     FILE*fp=fopen(temp,"w");
+//     active.pCount.lostOrAbandoned+=winLoss;
+//     fprintf(
+//         fp, 
+//         "%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\n",
+//         active.name,
+//         active.password,
+//         active.pScore.eTime,
+//         active.pScore.mTime,
+//         active.pScore.hTime,
+//         active.pCount.totalPlayed,
+//         active.pCount.totalwon,
+//         active.pCount.lostOrAbandoned
+//     );
+//     fclose(fp);
+// }
 
 void rst(){
     printf("\033[0;00m");
